ad15e011029bf39732c662512f542f20e84668c3
mzaini30 <muhzaini30@gmail.com>
2017-12-21 23:50:16 +0700

    oke

--- matriks.py
+++ matriks.py
@@ -1,14 +1,17 @@
-import numpy as np
+import numpy as np, os
 
 data = open('data.txt', 'r').read().splitlines()
 data = [a.split("\t") for a in data]
 
 data = np.array(data).T
-print data
+# print data
 
 hasil = open('hasil.txt', 'w')
 for a, b in enumerate(data):
 	for c, d in enumerate(data[a]):
-		print d
-		print '\t'
-	print '\n'
+		hasil.write(d)
+		hasil.write('\t')
+	hasil.write('\n')
+
+print 'Selesai.'
+os.system("pause")
